d93f4bf6294244bdb77e4d84a7482904c52fea9c
taggart-maher <taggart.talk@gmail.com>
2023-09-10 19:43:54 -0400

    Meows added

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 //initialize sounds for soundboard
 let SOUNDBOARD = {
+    "Happy" : "meow (6).m4a",
     "The Ambush" : "ambush.m4a",
     "Daddy Landlord" : "daddy_landlord.m4a",
     "Lemonless Lemonade" : "no_lemon.m4a",
+    "Standard" : "meow (8).m4a",
     "Monkey (Very straight)" : "straight_monkey.m4a",
+    "Angry" : "meow (3).m4a",
     "Gay Bars™" : "gay_bars.m4a",
+    "Fustrated" : "meow (1).m4a",
     "The Big Announcement" : "the_big_announcement",
     "Leftovers" : "leftovers.m4a",
+    "Disappointment" : "meow (2).m4a",
     "Chickfla Sauce" : "chickfla_sauce.m4a",
+    "Defensive" : "meow (7).m4a",
     "Glutenburg" : "glutenburg.m4a",
     "cuck screaming" : "cuck_scream.m4a"
-    
 }
 
 
