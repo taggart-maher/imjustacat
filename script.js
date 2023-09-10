75a5f1a603bacc30821dd8df32169fe076fc5cfb
taggart-maher <taggart.talk@gmail.com>
2023-09-10 16:34:03 -0400

    First sounds added

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 //initialize sounds for soundboard
 let SOUNDBOARD = {
-    og_meow_1 : new Audio("sounds/og_meow_1.m4a")
+    "The Ambush" : "ambush.m4a",
+    "Daddy Landlord" : "daddy_landlord.m4a",
+    "Lemonless Lemonade" : "no_lemon.m4a",
+    "Monkey (Very straight)" : "straight_monkey.m4a",
+    "Gay Bars™" : "gay_bars.m4a",
+    "The Big Announcement" : "the_big_announcement",
+    "Leftovers" : "leftovers.m4a",
+    "Chickfla Sauce" : "chickfla_sauce.m4a",
+    "Glutenburg" : "glutenburg.m4a",
+    "cuck screaming" : "cuck_scream.m4a"
+    
 }
 
 
-const playEffect = (soundboard_option) => {
-    SOUNDBOARD[soundboard_option].play();
+const playEffect = (sound_file) => {
+    const effect = new Audio(sound_file);
+    effect.play();
 }
+
+
+
+const main = () => {
+    const soundboard_node = document.getElementById("soundboard_primary");
+    const soundboard_keys = Object.keys(SOUNDBOARD);
+    const soundboard_size = soundboard_keys.length;
+    for (i=0; i<soundboard_size; i++){
+        const new_soundboard_btn = document.createElement("BUTTON");
+        new_soundboard_btn.className = 'soundboard-button';
+        const key = soundboard_keys[i];
+        new_soundboard_btn.innerHTML = key;
+        new_soundboard_btn.addEventListener('click', () => {
+            playEffect(`sounds/${SOUNDBOARD[key]}`);
+        });
+        soundboard_node.appendChild(new_soundboard_btn);
+
+    }
+}
+
+main();
